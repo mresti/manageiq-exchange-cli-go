210cf865551ff32c80bd757f75725849f5feea35
Esteban Dorado <fadoro@gmail.com>
2018-02-05 17:33:52 +0100

    Added tests for userCollection

--- src/manageiq-exchange/models/user/user_test.go
+++ src/manageiq-exchange/models/user/user_test.go
@@ -39,3 +39,50 @@ func TestUserPrint(t *testing.T) {
 		t.Errorf("User Print returned -%+v-, want -%+v-", user.Print(), want)
 	}
 }
+
+func TestUserCollectionInit(t *testing.T) {
+	user := User{
+		Login:    "aljesusg",
+		Name:     "Alberto",
+		GithubId: 1,
+	}
+	users := UserCollection{}
+	users.Users = append(users.Users, user)
+	users.Total = len(users.Users)
+
+	var data = map[string]interface{}{
+		"login":     "aljesusg",
+		"github_id": 1,
+		"name":      "Alberto",
+	}
+
+	var dataUsers = []interface{}{}
+	dataUsers = append(dataUsers, data)
+
+	var userCollection UserCollection
+	userCollection.Init(dataUsers)
+
+	if !reflect.DeepEqual(userCollection, users) {
+		t.Errorf("UserCollectionInit returned %+v, want %+v", userCollection, users)
+	}
+}
+
+func TestUserCollectionPrint(t *testing.T) {
+	want := fmt.Sprintf("\n\n%s (%d/%d)\n\n------------------------------------------\n\n", utils.PrintColor("Users", "Red"), 1, 1)
+	want += fmt.Sprintf("%s: aljesusg (1)\n\n", utils.PrintColor("User", "Red"))
+	want += fmt.Sprintf("    Name : Alberto\n\n\n")
+
+	user := User{
+		Login:    "aljesusg",
+		Name:     "Alberto",
+		GithubId: 1,
+	}
+	userCollection := UserCollection{}
+	userCollection.Users = append(userCollection.Users, user)
+	userCollection.Total = len(userCollection.Users)
+	gotUserCollection := userCollection.Print(1)
+
+	if !reflect.DeepEqual(gotUserCollection, want) {
+		t.Errorf("UserCollection Print returned -%+v-, want -%+v-", gotUserCollection, want)
+	}
+}
